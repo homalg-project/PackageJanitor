c97d4c4aabb8d1ec0568095040ff6f98fe0b3478
Fabian Zickgraf <f.zickgraf@dashdos.com>
2021-08-31 14:20:57 +0200

    Use "dispatch" for triggering CI

--- trigger_all_CIs.sh
+++ trigger_all_CIs.sh
@@ -12,10 +12,6 @@ while read repo; do
 	
 	echo $repo
 	
-	# get workflow run ID
-	workflow_run_id=$(curl -s --fail -H "Accept: application/vnd.github.v3+json" -H "Authorization: token $TOKEN" "https://api.github.com/repos/homalg-project/$repo/actions/runs?per_page=1&branch=master" | grep -m 1 '"id":' | grep -Po "\d+")
-	
-	# rerun the workflow run
-	curl --fail -X POST -H "Accept: application/vnd.github.v3+json" -H "Authorization: token $TOKEN" https://api.github.com/repos/homalg-project/$repo/actions/runs/$workflow_run_id/rerun
+	curl --fail -X POST -H "Accept: application/vnd.github.v3+json" -H "Authorization: token $TOKEN" https://api.github.com/repos/homalg-project/$repo/actions/workflows/Tests.yml/dispatches -d '{"ref": "master"}'
 	
 done <hosts
