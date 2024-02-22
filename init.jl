0266a708915938bd695b21b8c7024a4e7ee30c01
Fabian Zickgraf <f.zickgraf@dashdos.com>
2024-02-22 12:46:18 +0100

    Enhance init.jl

--- init.jl
+++ init.jl
@@ -1,7 +1,19 @@
+push!(ModulesForEvaluationStack, @__MODULE__)
+
+CAP.IS_PRECOMPILING = true
+
 include("init.g.autogen.jl")
 include("read.g.autogen.jl")
 
+CAP.IS_PRECOMPILING = false
+
+CAP_STATE = SAVE_CAP_STATE()
+
 function __init__()
+	
+	push!(ModulesForEvaluationStack, @__MODULE__)
+	
+	RESTORE_CAP_STATE(CAP_STATE)
 	
 	@init_CAP_package
 	
@@ -10,7 +22,7 @@ end
 # export all symbols
 # many thanks to https://discourse.julialang.org/t/exportall/4970/16
 for n in names(@__MODULE__; all=true)
-    if (Base.isidentifier(n) || n in [:⥉]) && n ∉ (Symbol(@__MODULE__), :eval, :include, :__init__)
+    if (Base.isidentifier(n) || n in [:⥉]) && n ∉ (Symbol(@__MODULE__), :eval, :include, :__init__, :CAP_STATE)
         @eval export $n
     end
 end
