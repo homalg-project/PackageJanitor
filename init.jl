746904cf1c7d4bcbb4ea73e9e48896d3bf77dce8
Fabian Zickgraf <f.zickgraf@dashdos.com>
2024-08-16 17:27:30 +0200

    Improve conversion to Julia

--- init.jl
+++ init.jl
@@ -40,7 +40,7 @@ end
 # export all symbols
 # many thanks to https://discourse.julialang.org/t/exportall/4970/16
 for n in names(@__MODULE__; all=true)
-    if (Base.isidentifier(n) || n in [:⥉]) && n ∉ (Symbol(@__MODULE__), :eval, :include, :__init__, :CAP_STATE)
+    if Base.isidentifier(n) && n ∉ (Symbol(@__MODULE__), :eval, :include, :__init__, :CAP_STATE)
         @eval export $n
     end
 end
